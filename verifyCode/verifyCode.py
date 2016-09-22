f330f53e3d798ac20807c0748600144070e14585
adolf2v <liuweiqiang2v@163.com>
2016-09-22 12:06:31 +0800

    解决报错问题

--- verifyCode/verifyCode.py
+++ verifyCode/verifyCode.py
@@ -7,7 +7,7 @@ date:2016-09-22
 需要下载汉字的字体库,chi-sim.traineddata，下载地址：https://pan.baidu.com/s/1pK7jNAj，解压后放到对应的tessdata文件夹即可
 用于测试的验证码图片:2.gif
 用户测试汉字的验证码图片:sim.png
-
+需要安装tesseract-ocr包的，要不然会报OSError: [Errno 2] No such file or directory的错
 """
 #从PIL库导入Image,用于生成Image对象
 from PIL import Image
